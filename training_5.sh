cb48ef44bbf50c2946d423ea33e5e8f5e994460c
Patrick Helber <patrickhelber@googlemail.com>
2018-08-14 03:13:15 +0200

    Create training_5.sh

---
+++ training_5.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sh ./local_test.sh "all_without_small_cities_512" 2 25000 0.001 8
+sh ./local_test.sh "all_without_sudan_512" 2 25000 0.001 8
+sh ./local_test.sh "kibera_and_lower_512" 2 25000 0.001 8
